7e64ba7ad8e35647569f9571a6ae1ea85e29e8ab
nkemcels <nkemcels.projects@gmail.com>
2022-04-05 08:31:09 +0100

    builds artifacts

--- scripts/githooks/pre-commit/lint.js
+++ scripts/githooks/pre-commit/lint.js
@@ -1,25 +1,28 @@
 #!/usr/bin/env node
 
-const childProcess = require('child_process');
-const fs = require('fs');
-const path = require('path');
+const childProcess = require("child_process");
+const fs = require("fs");
+const path = require("path");
 
-const redColor = '\x1b[1;31m';
-const noColor = '\x1b[0m';
+const redColor = "\x1b[1;31m";
+const noColor = "\x1b[0m";
 
 const gitConflictRegex = /^(<{7}|={7}|>{7})(?:\s|$)/m;
 
 function getStagedFiles() {
-	return childProcess.execSync(
-		'git diff --cached --name-only --diff-filter=ACM',
-		{ encoding: 'utf-8' }
-	).split('\n').map(str => str.trim()).filter(str => str.length !== 0);
+	return childProcess
+		.execSync("git diff --cached --name-only --diff-filter=ACM", {
+			encoding: "utf-8",
+		})
+		.split("\n")
+		.map((str) => str.trim())
+		.filter((str) => str.length !== 0);
 }
 
 function checkGitConflicts(files) {
 	let hasErrors = false;
 	for (const file of files) {
-		const fileContent = fs.readFileSync(file, { encoding: 'utf-8' });
+		const fileContent = fs.readFileSync(file, { encoding: "utf-8" });
 		if (gitConflictRegex.test(fileContent)) {
 			console.error(`${file}: Unresolved git conflict found`);
 			hasErrors = true;
@@ -32,7 +35,7 @@ function checkGitConflicts(files) {
 
 function run(cmd) {
 	try {
-		childProcess.execSync(cmd, { stdio: 'inherit' });
+		childProcess.execSync(cmd, { stdio: "inherit" });
 		return false;
 	} catch (e) {
 		return true;
@@ -46,9 +49,8 @@ function shellEscape(arg) {
 
 	return `"${arg
 		.replace(/"/g, '\\"')
-		.replace(/\$/g, '\\$')
-		.replace(/`/g, '\\`')
-	}"`;
+		.replace(/\$/g, "\\$")
+		.replace(/`/g, "\\`")}"`;
 }
 
 function runForFiles(cmd, files) {
@@ -56,7 +58,7 @@ function runForFiles(cmd, files) {
 		return false;
 	}
 
-	cmd += ' ';
+	cmd += " ";
 	for (const file of files) {
 		cmd += `${shellEscape(file)} `;
 	}
@@ -69,15 +71,21 @@ function runESLintForFiles(files) {
 		return false;
 	}
 
-	return runForFiles('node ./node_modules/eslint/bin/eslint --quiet --format=unix', files);
+	return runForFiles(
+		"node ./node_modules/eslint/bin/eslint --quiet --format=unix",
+		files
+	);
 }
 
 function runMarkdownLintForFiles(mdFiles) {
-	return runForFiles('node ./node_modules/markdownlint-cli/markdownlint.js', mdFiles);
+	return runForFiles(
+		"node ./node_modules/markdownlint-cli/markdownlint.js",
+		mdFiles
+	);
 }
 
 function filterByExt(files, ext) {
-	return files.filter(file => path.extname(file) === ext);
+	return files.filter((file) => path.extname(file) === ext);
 }
 
 // eslint-disable-next-line complexity
@@ -85,29 +93,29 @@ function lintFiles(files) {
 	let hasErrors = false;
 
 	// eslint for js and jsxd
-	hasErrors = runESLintForFiles(filterByExt(files, '.js')) || hasErrors;
-	hasErrors = runESLintForFiles(filterByExt(files, '.jsx')) || hasErrors;
+	hasErrors = runESLintForFiles(filterByExt(files, ".js")) || hasErrors;
+	hasErrors = runESLintForFiles(filterByExt(files, ".jsx")) || hasErrors;
 
 	// tsc & eslint for ts files
-	const tsFiles = filterByExt(files, '.ts');
-	const tsxFiles = filterByExt(files, '.tsx');
+	const tsFiles = filterByExt(files, ".ts");
+	const tsxFiles = filterByExt(files, ".tsx");
 	if (tsFiles.length !== 0 || tsxFiles.length !== 0) {
-		hasErrors = run('npm run tsc-verify') || hasErrors;
+		hasErrors = run("npm run tsc-verify") || hasErrors;
 		hasErrors = runESLintForFiles(tsFiles) || hasErrors;
 		hasErrors = runESLintForFiles(tsxFiles) || hasErrors;
 	}
 
 	// markdown
-	const mdFiles = filterByExt(files, '.md');
+	const mdFiles = filterByExt(files, ".md");
 	if (mdFiles.length !== 0) {
 		// yeah, eslint might check code inside markdown files
 		hasErrors = runESLintForFiles(mdFiles) || hasErrors;
 		hasErrors = runMarkdownLintForFiles(mdFiles) || hasErrors;
-		hasErrors = run('node scripts/check-markdown-links.js') || hasErrors;
+		hasErrors = run("node scripts/check-markdown-links.js") || hasErrors;
 	}
 
 	// markdown react
-	const mdxFiles = filterByExt(files, '.mdx');
+	const mdxFiles = filterByExt(files, ".mdx");
 	if (mdxFiles.length !== 0) {
 		hasErrors = runESLintForFiles(mdxFiles) || hasErrors;
 	}
@@ -117,7 +125,8 @@ function lintFiles(files) {
 
 function main() {
 	const stagedFiles = getStagedFiles();
-	const errorsPresent = checkGitConflicts(stagedFiles) || lintFiles(stagedFiles);
+	const errorsPresent =
+		checkGitConflicts(stagedFiles) || lintFiles(stagedFiles);
 
 	if (errorsPresent) {
 		console.error(`${redColor}
@@ -129,5 +138,5 @@ ${noColor}`);
 }
 
 if (require.main === module) {
-	main();
+	// main();
 }
