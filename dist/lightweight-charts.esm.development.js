483bb4df9ae6afd469c0d1981d20392169c5a70d
nkemcels <nkemcels.projects@gmail.com>
2022-04-11 10:50:42 +0100

    builds artifacts

--- dist/lightweight-charts.esm.development.js
+++ dist/lightweight-charts.esm.development.js
@@ -1,6 +1,6 @@
 /*!
  * @license
- * TradingView Lightweight Charts v3.8.0-dev+202204110845
+ * TradingView Lightweight Charts v3.8.0
  * Copyright (c) 2020 TradingView, Inc.
  * Licensed under Apache License 2.0 https://www.apache.org/licenses/LICENSE-2.0
  */
@@ -6384,7 +6384,6 @@ var TimeScale = /** @class */ (function () {
     TimeScale.prototype._internal_timeToIndex = function (time, findNearest) {
         if (this._private__points.length < 1) {
             // no time points available
-            console.log("[LW]: No time points are available");
             return null;
         }
         if (time._internal_timestamp > this._private__points[this._private__points.length - 1]._internal_time._internal_timestamp) {
@@ -12204,7 +12203,7 @@ function createChart(container, options) {
  * Returns the current version as a string. For example `'3.3.0'`.
  */
 function version() {
-    return "3.8.0-dev+202204110845";
+    return "3.8.0";
 }
 
 export { ColorType, CrosshairMode, LastPriceAnimationMode as LasPriceAnimationMode, LastPriceAnimationMode, LineStyle, LineType, PriceLineSource, PriceScaleMode, TickMarkType, TrackingModeExitMode, createChart, isBusinessDay, isUTCTimestamp, version };
